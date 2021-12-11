e22864996da0fc98a31dd5b7e5cab9d6ac784d1d
Murad_777 <murad.gadzhiev1999@mail.ru>
2021-12-11 22:49:55 +0300

    v0.1

---
+++ WPF2/Views/Pages/ActionPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF2.Context;
+using WPF2.Model;
+
+namespace WPF2.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ActionPage.xaml
+    /// </summary>
+    public partial class ActionPage : Page
+
+    {
+        public Book Book { get; set; }
+        public List<Author> Authors { get; set; }
+        public List<Genre> Genres { get; set; }
+        public List<Publisher> Publishers { get; set; }
+
+
+        public ActionPage(Book book)
+        {
+            InitializeComponent();
+            this.Book = book;
+            Authors = AppData.db.Author.ToList();
+            Genres = AppData.db.Genre.ToList();
+            Publishers = AppData.db.Publisher.ToList();
+            this.DataContext = this;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (Book.ID == 0)
+            {
+                AppData.db.Book.Add(Book);
+            }
+            AppData.db.SaveChanges();
+            MessageBox.Show("Данные сохранены!","Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+            NavigationService.GoBack();
+             
+        }
+    }
+}
---
+++ WPF2/Views/Pages/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF2.Context;
+using WPF2.Model;
+
+namespace WPF2.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataSource.ItemsSource = AppData.db.Book.ToList();
+        }
+
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ActionPage(new Model.Book()));
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var editBook = (Book)DataSource.SelectedItem;
+            if(editBook != null)
+            {
+                NavigationService.Navigate(new ActionPage(editBook));
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var deleteBook = (Book)DataSource.SelectedItem;
+            if (deleteBook != null)
+            {
+                AppData.db.Book.Remove(deleteBook);
+                AppData.db.SaveChanges();
+                MessageBox.Show("Данные удалены!", "Удаление", MessageBoxButton.OK, MessageBoxImage.Information);
+                Page_Loaded(null, null);
+            }
+        }
+    }
+}
